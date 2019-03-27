9c989ae671809af1740b8445f2d8843f619c184b
Five Grant <fivegrant@bennington.edu>
2019-03-27 02:07:31 +0000

    Changes to be committed: 	modified: run.py

--- run.py
+++ run.py
@@ -14,20 +14,67 @@ q = get_raw_adc_reading(bus)
 print(hex(q))
 print(convert_raw_reading(q, rg))
 print(convert_voltage_to_temp(convert_raw_reading(q, rg), 9800,5))
+temp = int(convert_voltage_to_temp(convert_raw_reading(q, rg), 9800,5))
 
 display = lights.Lineup((11, 13, 15, 16, 18))
+
 if sys.argv[1] == 'meter':
-    min, max = (4600, 17050)
-    
+    min, max = (0, 40)
+    limits = (min,(min + ((min+max)/2))/2,(min+max)/2, 
+     (max + ((min+max)/2))/2, max)
     while True:
+        temp = int(convert_voltage_to_temp(convert_raw_reading(\
+         get_raw_adc_reading(bus), rg), 9800,5))
+        for i in range(1,6):
+            display.darken(i)
+        generator = 1
+        for bar in limits:
+            if temp <= bar:
+                for i in range(1, generator):
+                    display.luminate(i)   
+                break
+            generator += 1
         
-elif sys.arg[1] == 'dark':
-    for i in range(6):
+elif sys.argv[1] == 'dark':
+    for i in range(1,6):
         display.darken(i)   
-     for i in range(6):
+    for i in range(1,6):
         display.luminate(i)
-        time.sleep(.23)
+        time.sleep(.3)
         display.darken(i)   
+
+elif sys.argv[1] == 'bulb':
+        which = int(sys.argv[2])
+        display.luminate(which)
+        time.sleep(1.5)
+        display.darken(which)   
     
 else:
     while True:
+        temp = int(convert_voltage_to_temp(convert_raw_reading(\
+         get_raw_adc_reading(bus), rg), 9800,5))
+        show = bin(temp)[2:]
+        print(show)
+        for i in range(1,6):
+            display.luminate(i)
+            time.sleep(.1)
+            display.darken(i)   
+        print('-')
+        for value in show:
+            print(value)
+            if int(value) == 1: 
+                display.luminate(1)
+                time.sleep(1)
+                display.darken(1)
+                time.sleep(1)
+            else:
+                display.luminate(2)
+                time.sleep(1)
+                display.darken(2)
+                time.sleep(1)
+        for i in range(1,6):
+            display.luminate(i)
+            time.sleep(.1)
+            display.darken(i)   
+
+
